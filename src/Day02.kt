697ea0b895cc4c98ae876206f58b7cb74e83edac
Alex <alexander.mironov@gmail.com>
2021-12-03 20:30:46 +0000

    Added solution for Day 3

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,39 +1,67 @@
+import kotlin.math.pow
+
 fun main() {
-    fun part1(input: List<String>): Long {
-        var horizontalPosition = 0L
-        var depth = 0L
-        for (statement in input) {
-            val (command, strValue) = statement.split(" ")
-            val value = strValue.toInt()
-            when (command) {
-                "forward" -> horizontalPosition += value
-                "up" -> depth -= value
-                "down" -> depth += value
-            }
+    fun part1(input: List<String>): Int {
+        val bitCount = input[0].length
+        val maxValue = 2.0.pow(bitCount).toInt() - 1
+        var gammaRateString = ""
+        for (i in 0 until bitCount) {
+            val mostFrequent = input.map { it[i] }.groupingBy { it }.eachCount().maxByOrNull { it.value }!!
+            gammaRateString += mostFrequent.key
         }
-        return horizontalPosition * depth
+        val gammaRate = gammaRateString.toInt(2)
+
+        return gammaRate * (maxValue - gammaRate)
     }
 
-    fun part2(input: List<String>): Long {
-        var horizontalPosition = 0L
-        var depth = 0L
-        var aim = 0L
-        for (statement in input) {
-            val (command, strValue) = statement.split(" ")
-            val value = strValue.toInt()
-            when (command) {
-                "forward" -> {
-                    horizontalPosition += value
-                    depth += (aim * value)
+
+    fun part2(input: List<String>): Int {
+        fun selectCommonOrDefault(frequencies: Map<Char, Int>, common: Common): Char {
+            return if (common == Common.MOST_COMMON) {
+                if (frequencies['0'] == frequencies['1']) {
+                    '1'
+                } else {
+                    frequencies.maxByOrNull { it.value }!!.key
                 }
-                "up" -> aim -= value
-                "down" -> aim += value
+            } else {
+                if (frequencies['0'] == frequencies['1']) {
+                    '0'
+                } else {
+                    frequencies.minByOrNull { it.value }!!.key
+                }
             }
         }
-        return horizontalPosition * depth
+
+        fun getRate(input: List<String>, common: Common): Int {
+            val bitCount = input[0].length
+            var pool = input.toMutableList()
+            var rate = 0
+            for (i in 0 until bitCount) {
+                val frequencies = pool.map { it[i] }.groupingBy { it }.eachCount()
+                val mostCommon = selectCommonOrDefault(frequencies, common)
+                pool = pool.filter { it[i] == mostCommon }.toMutableList()
+                if (pool.size == 1) {
+                    rate = pool[0].toInt(radix = 2)
+                    break
+                }
+            }
+            return rate
+        }
+
+        val oxygenGeneratorRate = getRate(input, Common.MOST_COMMON)
+        val co2ScrubberRate = getRate(input, Common.LEAST_COMMON)
+
+        return oxygenGeneratorRate * co2ScrubberRate
     }
 
-    val input = readInput("Day02")
+    val test = readInput("Day03_test")
+    check(part1(test) == 198)
+    check(part2(test) == 230)
+    val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
+}
+
+enum class Common {
+    MOST_COMMON, LEAST_COMMON
 }
